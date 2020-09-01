64355ec9996bcc869267b89ededbf2ee936e715d
GabrielNP <gabrielnp.2010@gmail.com>
2020-09-01 10:21:53 -0300

    Card shared component

--- alurapic/src/app/photos/photo-list/photo-list.module.ts
+++ alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { CardModule } from './../../shared/components/card/card.module';
 import { FilterByDescriptionPipe } from './filter-br-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { PhotoListComponent } from './photo-list.component';
@@ -15,6 +16,7 @@ import { PhotosComponent } from './photos/photos.component';
     LoadButtonComponent
   ],
   imports: [
+    CardModule,
     CommonModule,
     PhotoModule
   ]
