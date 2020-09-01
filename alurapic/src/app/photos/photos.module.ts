f14614460111ca254ba8e25da9640b07e52e4b9e
GabrielNP <gabrielnp.2010@gmail.com>
2020-09-01 09:26:18 -0300

    Paginator and load more button

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { FilterByDescriptionPipe } from './photo-list/filter-br-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
@@ -11,10 +12,11 @@ import { PhotosComponent } from './photo-list/photos/photos.component';
 @NgModule({
   declarations: [
     FilterByDescriptionPipe,
+    LoadButtonComponent,
     PhotoComponent,
     PhotoListComponent,
     PhotoFormComponent,
-    PhotosComponent
+    PhotosComponent,
   ],
   exports: [],
   imports: [
