2d10f58a4b0e711436773517c2dbeb7e582705c8
gabrielnp <gabrielnp.2010@gmail.com>
2021-03-03 20:56:22 -0300

    feat: add auth token interceptor
    CHANGE LOG:
    now we say to Angular do not use its own interceptor, but ours. The new interceptor automaticly controls token sending for every request (that needs auth) sent.

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from "@angular/core";
 import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from "./header/header.component";
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
     declarations: [ HeaderComponent ],
@@ -10,6 +12,9 @@ import { HeaderComponent } from "./header/header.component";
         CommonModule,
         RouterModule
     ],
-    exports: [ HeaderComponent ]
+    exports: [ HeaderComponent ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: RequestInterceptor, multi: true }
+    ]
 })
 export class CoreModule {}
